3c8c80c94dd9756b2f9e16d8f34e6a7c6575c7f0
Jonathan McMullan <jmcmullan92@outlook.com>
2020-08-15 22:46:26 +0100

    Section 5 - restructured folders and added functional components for NavBar, ActivityDashboard, ActivityList, ActivityDetails and ActivityForm. Set state for components to to view activities from the list in the details component and then edit details in the form component.

---
+++ web-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import { IActivity } from "../../../app/Models/activity";
+import { ActivityList } from "./ActivityList";
+import { ActivityDetails } from "../details/ActivityDetails";
+import { ActivityForm } from "../form/ActivityForm";
+
+interface IProps {
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  selectedActivity: IActivity | null;
+  editMode: boolean;
+  setEditMode: (editMode: boolean) => void;
+}
+
+export function ActivityDashboard({
+  activities,
+  selectActivity,
+  selectedActivity,
+  editMode,
+  setEditMode,
+}: IProps) {
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList activities={activities} selectActivity={selectActivity} />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        {selectedActivity && !editMode && (
+          <ActivityDetails activity={selectedActivity} setEditMode={setEditMode} />
+        )}
+        {editMode && <ActivityForm />}
+      </Grid.Column>
+    </Grid>
+  );
+}
